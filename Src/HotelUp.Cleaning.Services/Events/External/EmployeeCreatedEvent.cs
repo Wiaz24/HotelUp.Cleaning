b34a861fd733ce777c7e484b6e96b306db478331
Wiaz <wiazhub@gmail.com>
2025-01-26 20:26:05 +0100

    EmployeeCreatedEventHandler implemented

---
+++ Src/HotelUp.Cleaning.Services/Events/External/EmployeeCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using HotelUp.Cleaning.Services.Events.External.DTOs;
+
+// ReSharper disable once CheckNamespace
+namespace HotelUp.Employee.Services.Events;
+
+public record EmployeeCreatedEvent
+{
+    public required Guid Id { get; init; }
+    public required EmployeeType Role { get; init; }
+}
